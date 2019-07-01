32d35333a0ddf75d98816a2a5083964c81b8ad5f
KidusMT <kidusmamuye@gmail.com>
2019-07-01 03:45:17 +0300

    changed the toolbar theme style in the styles.xml file for the parent theme

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/mapbox/android/gestures/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/mapbox/android/gestures/R.java
@@ -93,23 +93,23 @@ public final class R {
         public static final int blocking = 0x7f090032;
         public static final int chronometer = 0x7f09003c;
         public static final int forever = 0x7f090071;
-        public static final int icon = 0x7f09007f;
-        public static final int icon_group = 0x7f090080;
-        public static final int info = 0x7f09008b;
-        public static final int italic = 0x7f090095;
-        public static final int line1 = 0x7f09009a;
-        public static final int line3 = 0x7f09009b;
-        public static final int normal = 0x7f0900be;
-        public static final int notification_background = 0x7f0900c2;
-        public static final int notification_main_column = 0x7f0900c3;
-        public static final int notification_main_column_container = 0x7f0900c4;
-        public static final int right_icon = 0x7f0900d8;
-        public static final int right_side = 0x7f0900d9;
-        public static final int tag_transition_group = 0x7f09012e;
-        public static final int text = 0x7f090131;
-        public static final int text2 = 0x7f090132;
-        public static final int time = 0x7f09013c;
-        public static final int title = 0x7f09013e;
+        public static final int icon = 0x7f090080;
+        public static final int icon_group = 0x7f090081;
+        public static final int info = 0x7f09008c;
+        public static final int italic = 0x7f090096;
+        public static final int line1 = 0x7f09009b;
+        public static final int line3 = 0x7f09009c;
+        public static final int normal = 0x7f0900bf;
+        public static final int notification_background = 0x7f0900c3;
+        public static final int notification_main_column = 0x7f0900c4;
+        public static final int notification_main_column_container = 0x7f0900c5;
+        public static final int right_icon = 0x7f0900d9;
+        public static final int right_side = 0x7f0900da;
+        public static final int tag_transition_group = 0x7f09012f;
+        public static final int text = 0x7f090132;
+        public static final int text2 = 0x7f090133;
+        public static final int time = 0x7f09013d;
+        public static final int title = 0x7f09013f;
     }
     public static final class integer {
         private integer() {}
